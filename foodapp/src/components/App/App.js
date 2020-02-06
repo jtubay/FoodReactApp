9fe89b30cbd5b1110f21ca6fd8d897cd72fddfa6
Jean Tubay <jean.tubay.92@gmail.com>
2020-02-05 22:57:12 -0500

    made component and added styles

--- foodapp/src/components/App/App.js
+++ foodapp/src/components/App/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Hello from "../../Hello"
 import Business from "../Business/Business.jsx"
 
+
 function App() {
   return (
     <Business />
--- foodapp/src/components/Business/Business.jsx
+++ foodapp/src/components/Business/Business.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './Business.css'
 
 
 const business = {
