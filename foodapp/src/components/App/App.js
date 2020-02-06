eb643c35d21522c9874a3cf6a5a8a659e27c4779
Jean Tubay <jean.tubay.92@gmail.com>
2020-02-05 22:46:46 -0500

    adding basic set up

--- foodapp/src/components/App/App.js
+++ foodapp/src/components/App/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
-import Hello from "./Hello.jsx"
+import Hello from "../../Hello"
+import Business from "../Business/Business.jsx"
 
 function App() {
   return (
-    <Hello />
+    <Business />
   );
 }
 
