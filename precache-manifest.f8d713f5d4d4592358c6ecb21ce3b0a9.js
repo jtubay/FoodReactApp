e979bf0421340125fbd79f610a8a8888769f1336
Jean Tubay <jean.tubay.92@gmail.com>
2020-02-05 21:00:18 -0500

    Updates

---
+++ precache-manifest.f8d713f5d4d4592358c6ecb21ce3b0a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3753fe70ab6ab39a5c7ab4b7a972abb3",
+    "url": "/FoodReactApp/index.html"
+  },
+  {
+    "revision": "797e54ad793657619e92",
+    "url": "/FoodReactApp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "79090732c5c3ee630bdb",
+    "url": "/FoodReactApp/static/js/2.39802714.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/FoodReactApp/static/js/2.39802714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "797e54ad793657619e92",
+    "url": "/FoodReactApp/static/js/main.a4508501.chunk.js"
+  },
+  {
+    "revision": "1b15717fafc87371e528",
+    "url": "/FoodReactApp/static/js/runtime-main.3b839a71.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/FoodReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
